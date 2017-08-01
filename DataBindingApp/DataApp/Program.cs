2c1496df8495664af3671afbac6a84ff7b3ea27b
David Barth <davidbarth80@gmail.com>
2017-08-01 22:14:03 +0100

    Add domain classes

--- DataBindingApp/DataApp/Program.cs
+++ DataBindingApp/DataApp/Program.cs
@@ -16,7 +16,7 @@ namespace DataApp
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new StartForm());
         }
     }
 }
